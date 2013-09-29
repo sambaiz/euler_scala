48db15deb310d4cdb6e212d18fe959b4cecbb977
sambaiz <sambaiz@sambaiz-Air.local>
2013-09-29 16:12:19 +0900

    add default arg

--- src/main.scala
+++ src/main.scala
@@ -1,5 +1,6 @@
 object Main {
   def main(args: Array[String]) = {
+    //println("sum 1 to 100 is " + util.sumIfTrue(1,(n:Int) => n<=100))
     Problem1.answer
     Problem2.answer
   }
@@ -11,9 +12,10 @@ package object impl {
 
 package object util {
   import impl._
-  
-  def sumIfTrue(num: Int, nonstop: (Int) => Boolean,
-      expr: (Int) => Boolean, next: (Int) => Int = (n: Int) => n+1): Int = 
+  def sumIfTrue(num: Int, 
+      nonstop: (Int) => Boolean, //Œp‘±‚·‚éðŒŽ®
+      expr: (Int) => Boolean = (n:Int) => true, //‰ÁŽZ‚·‚éðŒŽ®
+      next: (Int) => Int = (n: Int) => n+1): Int = //ŽŸ‚Ì’l‚Ì“ü—Í
     if(nonstop(num)) 
       expr(num) * num + sumIfTrue(next(num), nonstop, expr, next)
       else 0
